df267098d57ac4694e930cd816ce66b39c925ede
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-12-08 13:31:17 +0000

    execute functions on autocmd instead of when called

--- rplugin/python3/__init__.py
+++ rplugin/python3/__init__.py
@@ -8,13 +8,18 @@ class BalancedBrackets():
     def __init__(self, nvim):
         self.nvim = nvim
         self.brackets = Brackets()
-        #self.nvim.command("inoremap { {<CR>}<ESC>ko")
+        #self.nvim.feedkeys("inoremap { {<CR>}<ESC>ko")
 
-    @pynvim.function("HighlightImbalancedBrackets")
-    def highlight_imbalanced_brackets(self, args):
+    @pynvim.autocmd("InsertLeave")
+    def highlight_imbalanced_brackets(self):
         current_buffer = ''.join(self.nvim.current.buffer)
         if self.brackets.are_brackets_balanced(current_buffer):
             return
         highlight_coords = self.brackets.find_coords(self.nvim.current.buffer)
         for x, y in highlight_coords:
             self.nvim.current.buffer.add_highlight("Error", y, x, x+1)
+
+    @pynvim.autocmd("InsertCharPre", eval='v:char', sync=True)
+    def autocomplete_brackets(self, char):
+        if char in self.brackets.brackets.keys():
+            self.nvim.feedkeys(self.brackets.brackets[char], options="i")
