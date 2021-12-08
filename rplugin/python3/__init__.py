8371b3fa429959c8947018fbe34c1f437fc9c9d3
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-12-08 13:44:07 +0000

    adding src_id for add/remove highlights

--- rplugin/python3/__init__.py
+++ rplugin/python3/__init__.py
@@ -9,15 +9,17 @@ class BalancedBrackets():
         self.nvim = nvim
         self.brackets = Brackets()
         #self.nvim.feedkeys("inoremap { {<CR>}<ESC>ko")
+        self.highlight_src = self.nvim.new_highlight_source()
 
     @pynvim.autocmd("InsertLeave")
     def highlight_imbalanced_brackets(self):
+        self.nvim.current.buffer.clear_highlight(src_id=self.highlight_src)
         current_buffer = ''.join(self.nvim.current.buffer)
         if self.brackets.are_brackets_balanced(current_buffer):
             return
         highlight_coords = self.brackets.find_coords(self.nvim.current.buffer)
         for x, y in highlight_coords:
-            self.nvim.current.buffer.add_highlight("Error", y, x, x+1)
+            self.nvim.current.buffer.add_highlight("Error", y, x, x+1, src_id=self.highlight_src)
 
     @pynvim.autocmd("InsertCharPre", eval='v:char', sync=True)
     def autocomplete_brackets(self, char):
